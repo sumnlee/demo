8a8785a3aa85d313bfb643d114d94f6718518f0e
Sumanta_Mandal <73775840+sumnlee@users.noreply.github.com>
2025-11-09 12:35:28 +0530

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,5 @@
+# Second Assignment 
+a = input("Enter your first name:")
+b = input("Enter your second name:")
+
+print("hello,", a + " ", b ,"!","Welcome to the python Program")
---
+++ main.py
@@ -0,0 +1,13 @@
+# this is my first assignment
+a = input("Enter the first Number:")
+b = input("Enter the second Number:")
+
+a_1 = float(a)
+b_1 = float(b)
+
+print("addition is =",a_1 + b_1)
+print("Substraction is =",a_1 - b_1)
+print("Multiplicatio is =", a_1 * b_1)
+print("Division is = ",a_1 / b_1)
+
+print("---ThankYou---")
