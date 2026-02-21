5160f207688876cbd4b92967a46b2a9d63648175
Sumanta_Mandal <73775840+sumnlee@users.noreply.github.com>
2026-02-21 18:50:21 +0530

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,15 @@
+# Check if a Number is Even or Odd
+
+num = int(input("Please Enter a valid Number :"))
+
+if num % 2 == 0:
+    print("Number is Even Number")
+else:
+    print("Number is Odd ")
+
+print('Exit the program')
+
+
+
+
+
---
+++ task_2.py
@@ -0,0 +1,7 @@
+sum = 0
+for num in range(1, 51):
+    sum += num
+
+print("The sum of numbers from 1 to 50 is :", sum)
+
+print("Thank you!!!!")
